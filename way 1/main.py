57cd4043fb9d1a5386810feb8521c84dea22fd5d
SNUcoder <98022575+SNUcoder@users.noreply.github.com>
2022-07-17 19:25:37 +0530

    Initial commit

---
+++ way 1/main.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+print("Sum of 2 numbers!\n")
+
+input("Enter your 1st number: ")
+input("Enter your 2nd number: ")
+
+print("Your result is.....")
+webbrowser.open("https://www.youtube.com/watch?v=oHg5SJYRHA0")
+
+time.sleep(6)
+print("As long as trolls are still trolling, the Rick will never stop rolling.")
