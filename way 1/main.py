327484bba83f91dec0e9b8d44c51e1fb0486d42e
SNUcoder <98022575+SNUcoder@users.noreply.github.com>
2022-07-17 19:30:04 +0530

    Initial commit

--- way 1/main.py
+++ way 1/main.py
@@ -1,13 +1,14 @@
-import webbrowser
-import time
+import webbrowser #import webbrowser to open the rickroll page
+import time #import time
 
 print("Sum of 2 numbers!\n")
 
-input("Enter your 1st number: ")
-input("Enter your 2nd number: ")
+input("Enter your 1st number: ") # ask the first number but dosen't store it in any variable
+input("Enter your 2nd number: ") # ask the second number but dosen't store it in any variable
 
-print("Your result is.....")
-webbrowser.open("https://www.youtube.com/watch?v=oHg5SJYRHA0")
+print("Your result is.....") # gives a little suspense to give the result
+webbrowser.open("https://www.youtube.com/watch?v=oHg5SJYRHA0") # opens the rickroll page
 
-time.sleep(6)
-print("As long as trolls are still trolling, the Rick will never stop rolling.")
+time.sleep(6) # waits for a few seconds
+
+print("As long as trolls are still trolling, the Rick will never stop rolling.") # prints the great slogan
